2f747004ff17ed0b6b3ed93493f85cc1ae172017
Thao Truong <thao.truong@arthaus.co.uk>
2020-06-09 17:32:25 +0100

    changed /public/images

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options: { path: `${__dirname}/public/images/`, name: `imgFromNetlify` },
+      options: { path: `${__dirname}/public/images`, name: `imgFromNetlify` },
     },
     {
       resolve: "gatsby-plugin-html-attributes",
