63c68865fcc1d6c236bd91473ef6544b96cc7176
Thao Truong <thaotruong@Thaos-iMac.local>
2020-09-22 10:54:51 +0100

    changed redirect url

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React, { useEffect } from "react"
 export default function Home() {
   useEffect(() => {
     window &&
-      window.location.replace("https://www.arthaus.co.uk/toolkit/toolkit.html")
+      window.location.replace(
+        "https://www.arthaus.co.uk/insights/insights.html"
+      )
   }, [])
   return (
     <div>
-      <h1>Redirect to ToolKit page...</h1>
+      <h1>Redirect to Insights page...</h1>
     </div>
   )
 }
