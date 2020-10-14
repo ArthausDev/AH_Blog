c17aca19960bfb887664e8415e4e91da8fa22e5d
Thao Truong <thaotruong@Thaos-iMac.local>
2020-10-14 12:21:06 +0100

    removed redirect on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react"
 
 export default function Home() {
-  useEffect(() => {
-    window &&
-      window.location.replace(
-        "https://www.arthaus.co.uk/insights/insights.html"
-      )
-  }, [])
+  // useEffect(() => {
+  //   window &&
+  //     window.location.replace(
+  //       "https://www.arthaus.co.uk/insights/insights.html"
+  //     )
+  // }, [])
   return (
     <div>
-      <h1>Redirect to Insights page...</h1>
+      <h1>Visit <a href='https://www.arthaus.co.uk/insights/insights.html'>Arthaus Insights page</a></h1>
     </div>
   )
 }
